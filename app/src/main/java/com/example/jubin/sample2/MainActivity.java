8dee02d4d374aebed51165aef7e2cf404096e6ca
jubzzzz1234 <cpjubin@gmail.com>
2017-06-21 17:05:55 +0530

    textview added

--- app/src/main/java/com/example/jubin/sample2/MainActivity.java
+++ app/src/main/java/com/example/jubin/sample2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.jubin.sample2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //making changes in branch
+        TextView textView=(TextView)findViewById(R.id.text_view);
+        String string="change made";
+        textView.setText(string);
     }
 }
